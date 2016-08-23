b2c808234e0fdce1de3025394f554bda01189644
yun <ll86115@hotmail.com>
2016-08-23 15:06:34 +0800

    0823-2

--- resources/views/project.blade.php
+++ resources/views/project.blade.php
@@ -1,62 +1,91 @@
 @extends('layout')
 
+@section('custom_css')
+
+<link rel="stylesheet" type="text/css" href="{{asset('css/project.css')}}">
+
+@endsection
+
 @section('content')
-	<section style="height: auto;">
-		<div style="margin-bottom: 60px;">
-			<h2 style="font-weight: bold; margin-bottom: 30px;">Basic Information</h2>
-			<p>{{ $project->name}}</p>
+	<section style="height: 125px; background-color:#dcdcdc;"></section>
+
+	<section class="sec-norm">
+		<div class="conwidth">
+			<h1>{{ $project->name}}</h1>
 			<p>{{ $project->intro}}</p>
-			<p>{{ $project->description}}</p>
-			<p>{{ $project->video_url}}</p>
-			<p>{{ $project->perform_date}}</p>
+			<p>&#8212; 2014 &#8212;</p>
 		</div>
-		
 	</section>
 
-	<section style="height: auto;">
+	<section class="sec-img">
 		<div>
-			<h2 style="font-weight: bold; margin-bottom: 30px;">Reviews</h2>
+			@foreach ($images as $image)
+				<img src="{{ asset($image->url)}}" alt="">
+			@endforeach
+		</div>	
+	</section>
+
+	<section class="sec-norm">
+		<div class="conwidth">
+			<h2>&#8212; Information &#8212;</h2>
+			<p class="text-lft">{{ $project->description}}</p>
+		</div>
+	</section>
+
+	<section>
+		<div>
+			<iframe width="70%" height="450px" src="//{{ $project->video_url}}?rel=0&modestbranding=1&autohide=1&showinfo=0&controls=0" frameborder="0" allowfullscreen></iframe>
+		</div>
+	</section>
+
+	<section class="sec-norm underline">
+			<h2>&#8212; Reviews &#8212;</h2>
 			@foreach ($reviews as $review)
-				<div style="margin-bottom: 30px;">
-					<p>{{ $review->content}}</p>
-					<p>{{ $review->reviewer}}</p>
+				<div class="conwidth" style="margin-bottom: 70px;">
+					<p class="text-lft">{{ $review->content}}</p>
+					<h4>&#8212; {{ $project->perform_date}} {{ $review->reviewer}}</h4>
 				</div>
 			@endforeach
-		</div>	
 	</section>
 
-	<section style="height: auto; margin-bottom: 50px">
-		<div>
-			<h2 style="font-weight: bold; margin-bottom: 30px;">Project Photos</h2>
-			@foreach ($images as $image)
-				<img src="{{ asset($image->url)}}" alt="" style="width: 250px; display: inline-block">
-			@endforeach
-		</div>	
-	</section>
-
-	<section style="height: auto; margin-bottom: 50px">
-		<div>
-			<h2 style="font-weight: bold; margin-bottom: 30px;">Main Stuff</h2>
+	<section class="sec-norm">
+		<div class="conwidth">
+			<h2>&#8212; Main Staff &#8212;</h2>
 			@foreach ($main_stuff as $stuff)
-				<div style="width: 250px; display: inline-block">
-					<div>
-						<img src="{{ asset($stuff->photo)}}" alt="" style="width: 250px;">
+				<div style="width: 200px; display: inline-block">
+					<div class="staffoto">
+						<img src="{{ asset($stuff->photo)}}" alt="">
 					</div>
-					<p>{{$stuff->name}}</p>
+					<h3>{{$stuff->name}}</h3>
 					<p>{{$stuff->role}}</p>
 				</div>
-				
 			@endforeach
 		</div>	
 	</section>
 
-	<section style="height: auto;">
-		<div>
-			<h2 style="font-weight: bold; margin-bottom: 30px;">Stuff</h2>
+	<section class="sec-norm">
+		<div class="conwidth">
+			<h2>&#8212; Staff &#8212;</h2>
 			@foreach ($other_stuff as $stuff)
 				<p>{{ $stuff->name}} - {{$stuff->role}}</p>
 			@endforeach
+			<p>Tommy Lin - Assistant / Tommy Lin - Assistant / Tommy Lin - Assistant</p>
 		</div>	
 	</section>
-	
+
+	<section class="sec-norm">
+		<div class="proj-btm"><a href="/works"><p>&#8212; All Works &#8212;</p></a></div>
+		<div class="proj-btm"><a href=""><p>&#8212; Back to Top &#8212;</p></a></div>
+	</section>
+
+	<section class="onepage-foot">
+			<div class="con1">
+				<footer>
+					<p class="copyright">Copyright &copy; 2016 B.DANCE . All rights reserved.</p>
+				</footer>
+			</div>
+	</section>
+@endsection
+@section('footer')
+
 @endsection
