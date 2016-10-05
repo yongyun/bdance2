95f1381fa17ff750bf70c2a0411d5a215a352142
yun <ll86115@hotmail.com>
2016-10-05 17:59:31 +0800

    1005

--- resources/views/bpost.blade.php
+++ resources/views/bpost.blade.php
@@ -1,7 +1,6 @@
 @extends('layout')
 
 @section('custom_css')
-<link rel="stylesheet" href="http://cdn.static.runoob.com/libs/bootstrap/3.3.7/css/bootstrap.min.css">
 <link rel="stylesheet" type="text/css" href="{{asset('plugins/jquery.bxslider/jquery.bxslider.css')}}">
 <link rel="stylesheet" type="text/css" href="{{ asset('css/about.css')}}" />
 <link rel="stylesheet" type="text/css" href="{{asset('css/animate.css')}}">
@@ -11,11 +10,28 @@
 @endsection
 
 @section('custom_js')
-<script src="http://cdn.static.runoob.com/libs/jquery/2.1.1/jquery.min.js"></script>
-<script src="http://cdn.static.runoob.com/libs/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+<script src="http://code.jquery.com/jquery-1.7.2.min.js"></script>
 <script src="{{ asset('plugins/film_roll/js/jquery.film_roll.min.js')}}"></script>
 <script src="{{ asset('plugins/jquery.bxslider/jquery.bxslider.min.js')}}"></script>
 <script>
+	$(document).ready(function() {
+    $("#content").find("[id^='tab']").hide(); // Hide all content
+    $("#tabs li:first").attr("id","current"); // Activate the first tab
+    $("#content #tab1").fadeIn(); // Show first tab's content
+    
+    $('#tabs a').click(function(e) {
+        e.preventDefault();
+        if ($(this).closest("li").attr("id") == "current"){ //detection for current tab
+         return;       
+        }
+        else{             
+          $("#content").find("[id^='tab']").hide(); // Hide all content
+          $("#tabs li").attr("id",""); //Reset id's
+          $(this).parent().attr("id","current"); // Activate this
+          $('#' + $(this).attr('name')).fadeIn(); // Show content for the current tab
+        }
+    });
+});
 	$(function() {
 		var awardSlider = $('#awardSlider').bxSlider({	
 			captions: true
@@ -27,6 +43,7 @@
 			$('html, body').animate({scrollTop : 0}, 1000);
 			return false;
 		});
+
 </script>
 @endsection
 
@@ -42,9 +59,9 @@
 					<li><img src="/img/Francesca FoscariniLena Meyer.jpg" width="100%" title="Grandmother by Francesca Foscarini"></li>	
 					<li><img src="/img/Francesca Foscarini_Sara Wiktorowicz.jpg" width="100%" title="Grandmother by Francesca Foscarini"></li>	
 					<li><img src="/img/Francesca Foscarini@ Lena Meyer.jpg" width="100%" title="Grandmother by Francesca Foscarini"></li>	
-					<li><img src="/img/Idan Sharabi _Your2Tami Weiss Photography.jpg" width="100%" title="Your Name by Idan Sharabi"></li>
-					<li><img src="/img/Idan Sharabi _Your3Tami Weiss Photography.jpg" width="100%" title="Your Name by Idan Sharabi"></li>
-					<li><img src="/img/Idan Sharabi _YourTami Weiss Photography.jpg" width="100%" title="Your Name by Idan Sharabi"></li>	
+					<li><img src="/img/Idan Sharabi _Your2Tami Weiss Photography.jpg" width="100%" title="Your by Idan Sharabi"></li>
+					<li><img src="/img/Idan Sharabi _Your3Tami Weiss Photography.jpg" width="100%" title="Your by Idan Sharabi"></li>
+					<li><img src="/img/Idan Sharabi _YourTami Weiss Photography.jpg" width="100%" title="Your by Idan Sharabi"></li>	
 					<li><img src="/img/Then, Before, Now, Once moreAntonin Comestaz.jpg" width="100%" title="THEN, BEFORE, NOW, ONCE MORE  by Antonin Comestaz"></li>	
 					<li><img src="/img/Then, Before, Now, Once moreAntonin Comestaz2.jpg" width="100%" title="THEN, BEFORE, NOW, ONCE MORE  by Antonin Comestaz"></li>
 					<li><img src="/img/Then, Before, Now, Once moreInbal Cohen Hamo.jpg" width="100%" title="THEN, BEFORE, NOW, ONCE MORE  by Antonin Comestaz"></li>
@@ -90,37 +107,37 @@
 						<h2>&#8212; Artist &#8212;</h2>
 				</div>
 				<div class="boomhistory artist">
-					<ul>
+					<ul id="tabs">
 						<li>
-							<a href="">
+							<a href="#" name="tab1">
 								<img src="/img/Tsai2.jpg">
 								<p>Po-Cheng Tsai</p>
 								<h1>Taiwan</h1>
 							</a>
 						</li>
 						<li>
-							<a href="">
+							<a href="#" name="tab2">
 								<img src="/img/IdanSharab.jpg">
 								<p>Idan Sharab</p>
 								<h1>Israel</h1>
 							</a>
 						</li>
 						<li>
-							<a href="">
+							<a href="#" name="tab3">
 								<img src="/img/FURIOGANZ.jpg">
 								<p>Francesca Foscarini</p>
 								<h1>Italiy</h1>
 							</a>
 						</li>
 						<li>
-							<a href="">
+							<a href="#" name="tab4">
 								<img src="/img/AntoninComestaz.jpg">
 								<p>Antonin Comestaz</p>
 								<h1>France</h1>
 							</a>
 						</li>
 						<li>
-							<a href="">
+							<a href="#" name="tab5">
 								<img src="/img/511803.jpg">
 								<p>Joeri Alexander Dubbe</p>
 								<h1>Haarlem</h1>
@@ -131,23 +148,108 @@
 				<div class="atext artist">
 						<h2>&#8212; Artist Info &#8212;</h2>
 				</div>
-				<div class="atext at-left" style="margin: 30px auto">
-					<p>Versatile independent choreographer and dancer. Her personal career was marked by the encounter with quite important masters of the national and international dance scene, with whom she had the opportunity to research on the languages of contemporary dance and improvisation, thus enriching and internalizing her language both in terms of expression and of performance. She started her own professional experience from 2000. Also Francesca is a winner of the Equilibrio Award for Performer in 2013. As well as she won the jury prize of the 17th edition of the MASDANZA in 2012. She has been selected as the young performer in 2012.	
-					</p>
-					<h3>Concept of work</h3>
-					<p>GRANDMOTHER</p>
-					<p>A body letting go of the past, The urgency of being present in the here and now, Never knowing what will happen next, Moving by the rhythm of breath, memories, and light conquering darkness.
-					</p>
-					<div  class="btext">
-						<div class="bcon2">
-							<p>Duration &#58; 10mins</p>
-							<p>Choreography & Performance &#58; Francesca Foscarini</p>
-							<p>Technician &#58; Luca Serafini</p>
-							<p>Photographer &#58; Luca Serafini</p>
-							<a href=""><h6>Artist Link</h6></a>
+				<div id="content"> 
+				    <div id="tab1" class="atext at-left" style="margin: 30px auto">
+				    	<p>Po-Cheng Tsai was born in Taiwan in 1987. He graduated from
+
+Tsoying Senior High School and further studied in Taipei
+
+National University of the Arts. He was selected to represent
+
+Taiwan as a choreographer in the Asian Young Choreographer
+
+Project. In 2014, Tsai founded his own company B.DANCE in
+
+Taiwan. He also worked as guest choreographer with different professional dance companies,
+
+including Stuttgart Gauthier Dance in Germany, Tanz Luzerner Theater in Switzerland, Cloud Gate
+
+2 and Kaohsiung City Ballet in Taiwan.
+
+Between 2007 and 2014 he created several choreography works for following dance company and
+
+dance schools:
+
+• Kaohsiung City Ballet: Record of Memories (2006), Heaven (2007), Winter (2008), Lunar
+
+Eclipse (2010), Heart Break (2013) and Stars (2014).
+
+• Taipei Physical Education College (TPEC): Stream (2013);
+
+• Tsoying Senior High School (TSHS): Pulse (2009), Eclipse of Night (2011) and Aurora
+
+(2012);
+
+• Zhongzheng Senior High School (ZZSH): Pulse (2009) and Split (2011).
+						</p>
+						<h2>Concept of work</h2>
+						<p>GRANDMOTHER</p>
+						<p>A body letting go of the past, The urgency of being present in the here and now, Never knowing what will happen next, Moving by the rhythm of breath, memories, and light conquering darkness.
+						</p>
+						<div  class="btext">
+							<div class="bcon2">
+								<p>Duration &#58; 10mins</p>
+								<p>Choreography & Performance &#58; Francesca Foscarini</p>
+								<p>Technician &#58; Luca Serafini</p>
+								<p>Photographer &#58; Luca Serafini</p>
+								<a href=""><h6>Artist Link</h6></a>
+							</div>
 						</div>
-					</div>
+				    </div>
+				    <div id="tab2" class="atext at-left" style="margin: 30px auto">
+				    	<p>Hello
+						</p>
+						<h3>Concept of work</h3>
+						<p>GRANDMOTHER</p>
+						<p>A body letting go of the past, The urgency of being present in the here and now, Never knowing what will happen next, Moving by the rhythm of breath, memories, and light conquering darkness.
+						</p>
+						<div  class="btext">
+							<div class="bcon2">
+								<p>Duration &#58; 10mins</p>
+								<p>Choreography & Performance &#58; Francesca Foscarini</p>
+								<p>Technician &#58; Luca Serafini</p>
+								<p>Photographer &#58; Luca Serafini</p>
+								<a href=""><h6>Artist Link</h6></a>
+							</div>
+						</div>
+				    </div>
+				    <div id="tab3" class="atext at-left" style="margin: 30px auto">
+				    	<p>Hello
+						</p>
+						<h3>Concept of work</h3>
+						<p>GRANDMOTHER</p>
+						<p>A body letting go of the past, The urgency of being present in the here and now, Never knowing what will happen next, Moving by the rhythm of breath, memories, and light conquering darkness.
+						</p>
+						<div  class="btext">
+							<div class="bcon2">
+								<p>Duration &#58; 10mins</p>
+								<p>Choreography & Performance &#58; Francesca Foscarini</p>
+								<p>Technician &#58; Luca Serafini</p>
+								<p>Photographer &#58; Luca Serafini</p>
+								<a href=""><h6>Artist Link</h6></a>
+							</div>
+						</div>
+				    </div>
+				    <div id="tab4" class="atext at-left" style="margin: 30px auto">
+				    	<p>Hello
+						</p>
+						<h3>Concept of work</h3>
+						<p>GRANDMOTHER</p>
+						<p>A body letting go of the past, The urgency of being present in the here and now, Never knowing what will happen next, Moving by the rhythm of breath, memories, and light conquering darkness.
+						</p>
+						<div  class="btext">
+							<div class="bcon2">
+								<p>Duration &#58; 10mins</p>
+								<p>Choreography & Performance &#58; Francesca Foscarini</p>
+								<p>Technician &#58; Luca Serafini</p>
+								<p>Photographer &#58; Luca Serafini</p>
+								<a href=""><h6>Artist Link</h6></a>
+							</div>
+						</div>
+				    </div>
+				    <div id="tab4">4</div>
 				</div>
+				
 			</div>
 	</section>
 
