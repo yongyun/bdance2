3d74cb231445805fc68f0c8c1732554e04b5abb1
yun <ll86115@hotmail.com>
2016-11-29 11:07:44 +0800

    Back End - edit the menu lists

--- public/bdance_admin/inc/web_info.php
+++ public/bdance_admin/inc/web_info.php
@@ -6,17 +6,17 @@ define('SYS_COOKEY1','bdance_admin_token');
 $su_agent = $_SERVER['HTTP_USER_AGENT'];
 
 //MAIN
-$main_sys[0] = '首頁資訊';
+$main_sys[0] = '首頁資訊Index';
 $main_url_sys[0] = 'index_sys.php';
-$main_sys[1] = '關於我們';
+$main_sys[1] = '關於我們About';
 $main_url_sys[1] = 'about_sys.php';
-$main_sys[2] = '最新消息';
+$main_sys[2] = '最新消息News';
 $main_url_sys[2] = 'news_sys.php';
-$main_sys[3] = '作品';
+$main_sys[3] = '作品Works';
 $main_url_sys[3] = 'works_sys.php';
-$main_sys[4] = '售票資訊';
+$main_sys[4] = 'BOOM';
 $main_url_sys[4] = 'boom_sys.php';
-$main_sys[5] = '聯絡我們';
+$main_sys[5] = '聯絡我們Contact';
 $main_url_sys[5] = 'message_sys.php';
 
 ?>
