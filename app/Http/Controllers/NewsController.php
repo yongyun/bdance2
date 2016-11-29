50c540ec2dd05be13a25d60fd52189bc5f2329ae
sakebboy <sakebboy@hotmail.com>
2016-11-29 09:48:31 +0800

    修正 news大小寫問題

--- app/Http/Controllers/NewsController.php
+++ app/Http/Controllers/NewsController.php
@@ -5,16 +5,16 @@ namespace App\Http\Controllers;
 use Request;
 use App\User;
 use App\Http\Controllers\Controller; 
-use App\News;
-use App\News_ad;
-use App\News_video;
+use App\news;
+use App\news_ad;
+use App\news_video;
 
 
 class NewsController extends Controller 
 {
 
 	public function allNews() {
-		$news_list = News::where([
+		$news_list = news::where([
 				['nw_del','=',0],
 				['nw_status','=','0']
 			])->orderBy('nw_id','desc')
@@ -24,12 +24,12 @@ class NewsController extends Controller
 	}
 
 	public function showNews($id) {
-		$news_list = News::where([
+		$news_list = news::where([
 				['nw_del','=',0],
 				['nw_id','=',$id]
 			])->get();
 			
-		$news_ad = News_ad::where([
+		$news_ad = news_ad::where([
 				['na_del','=',0],
 				['na_nwid','=',$id]
 			])->get();
@@ -38,15 +38,15 @@ class NewsController extends Controller
 				['nv_nwid','=',$id]
 			])->get();
 
-		$news_pr = News::where([
+		$news_pr = news::where([
 				['nw_del','=',0],
 			])->orderBy('nw_id','asc')->limit(1)->get();
 
-		$news_nt = News::where([
+		$news_nt = news::where([
 				['nw_del','=',0],
 			])->orderBy('nw_id','desc')->limit(1)->get();
 		
-		$news_next = News::where([
+		$news_next = news::where([
 				['nw_del','=',0],
 				['nw_id','>=',$id]
 			])->orderBy('nw_id','asc')->limit(2)->get();
