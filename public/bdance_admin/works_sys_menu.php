102e2489a520c0ad4966b658291d1f6e14779749
sakebboy <sakebboy@hotmail.com>
2016-12-05 01:03:23 +0800

    更新menu

--- public/bdance_admin/works_sys_menu.php
+++ public/bdance_admin/works_sys_menu.php
@@ -7,15 +7,15 @@
             <h2><i class="icon-th-list"></i> 顯示切換</h2>
         </div>
         <div class="box-content" style="display: block;">
-            <a class="btn btn-outline-success" href="works_sys_content1.php?id=<?php echo $id;?>">封面內容</a>
-			<a class="btn btn-outline-success" href="works_sys_content2.php?id=<?php echo $id;?>">輪播圖片</a>
-			<a class="btn btn-outline-success" href="works_sys_content3.php?id=<?php echo $id;?>">內容訊息</a>
-			<a class="btn btn-outline-success" href="works_sys_content9.php?id=<?php echo $id;?>">獎項</a>
-			<a class="btn btn-outline-success" href="works_sys_content4.php?id=<?php echo $id;?>">細節</a>
-			<a class="btn btn-outline-success" href="works_sys_content5.php?id=<?php echo $id;?>">演出日期</a>
-			<a class="btn btn-outline-success" href="works_sys_content6.php?id=<?php echo $id;?>">影片設定</a>
-			<a class="btn btn-outline-success" href="works_sys_content7.php?id=<?php echo $id;?>">評論</a>
-			<a class="btn btn-outline-success" href="works_sys_content8.php?id=<?php echo $id;?>">成員設定</a>
+            <a class="btn btn-outline-success" href="works_sys_content1.php?id=<?php echo $id;?>">作品設定Project Setting</a>
+			<a class="btn btn-outline-success" href="works_sys_content2.php?id=<?php echo $id;?>">輪播Slideshow</a>
+			<a class="btn btn-outline-success" href="works_sys_content3.php?id=<?php echo $id;?>">作品介紹Info</a>
+			<a class="btn btn-outline-success" href="works_sys_content9.php?id=<?php echo $id;?>">獎項Awards</a>
+			<a class="btn btn-outline-success" href="works_sys_content4.php?id=<?php echo $id;?>">細節Details</a>
+			<a class="btn btn-outline-success" href="works_sys_content5.php?id=<?php echo $id;?>">巡迴演出資訊Tour Date</a>
+			<a class="btn btn-outline-success" href="works_sys_content6.php?id=<?php echo $id;?>">影片Video</a>
+			<a class="btn btn-outline-success" href="works_sys_content7.php?id=<?php echo $id;?>">評論Reviews</a>
+			<a class="btn btn-outline-success" href="works_sys_content8.php?id=<?php echo $id;?>">表演者與工作人員Staff</a>
         </div>  
 	</div>
 </div>
